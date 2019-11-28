52abba0822ca738922b0ab50b96dc8d9536c54de
HMSBennett <harrisonsbennett@gmail.com>
2019-11-28 11:41:30 +0000

    Inital set up

---
+++ docker.sh
@@ -0,0 +1,17 @@
+echo Docker Install
+
+git clone https://github.com/HMSBennett/DockerInstall
+
+cd DockerInstall
+
+./install.sh
+
+echo Build Image
+
+cd HelloWorld
+
+docker build -t helloworld:v1 .
+
+echo Run Nginx/Page
+
+docker run -d -p 8081:80 helloworld:v1
